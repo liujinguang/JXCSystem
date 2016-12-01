e6bd5a9404057b9c08a9a54f03dfefd5e2934181
liujinguang <jinguang.liu@qq.com>
2016-12-01 22:59:19 +0800

    1. move resources to src/main/resource 2. add Utils.java and replace related moudles

--- JCXSystemPro/src/main/java/com/lzw/login/LoginPanel.java
+++ JCXSystemPro/src/main/java/com/lzw/login/LoginPanel.java
@@ -1,22 +1,30 @@
-﻿package com.lzw.login;
+package com.lzw.login;
+
 import java.awt.Graphics;
 import java.awt.Image;
 
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
+
+import com.lzw.Utils.Utils;
+
+@SuppressWarnings("serial")
+
 public class LoginPanel extends JPanel {
-	protected ImageIcon icon;
-	public int width,height;
-	public LoginPanel() {
-		super();
-		icon = new ImageIcon("res/login.jpg");
-		width = icon.getIconWidth();
-		height = icon.getIconHeight();
-		setSize(width, height);
-	}
-	protected void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		Image img = icon.getImage();
-		g.drawImage(img, 0, 0,getParent());
-	}
+    protected ImageIcon icon;
+    public int width, height;
+
+    public LoginPanel() {
+        super();
+        icon = new ImageIcon(Utils.getResourceURL("login.jpg"));
+        width = icon.getIconWidth();
+        height = icon.getIconHeight();
+        setSize(width, height);
+    }
+
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Image img = icon.getImage();
+        g.drawImage(img, 0, 0, getParent());
+    }
 }
